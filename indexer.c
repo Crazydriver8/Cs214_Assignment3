352c0bb7c9420d4a6c0e48b1191a3c7605f158b5
Crazydriver8 <krazydriver8@gmail.com>
2014-10-19 15:45:51 -0400

    work on io

--- indexer.c
+++ indexer.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 #include "indexer.h"
 //done no compile errors
 Index *IndexCreate(char* indexName) {
@@ -46,41 +47,41 @@ int IndexInsert(Index *indx, char* tk, char* fileName) {
 	
 	//this should set the previous node pointer to the token node
 	//the bucket in the hash table is pointing at
-	tkNode* previous = tkNode* indx->Array[indxPos];
-    	
-	fileNode* prevFile = previous->fileNodePTR;
+	tkNode *current = &indx->Array[indxPos];
+    	int a;
+	fileNode* prevFile = current->fileNodePTR;
 	//if there is only one token node in the list
-	if (previous != NULL && previous->next == NULL) {
-		int a = strcmp(previous->tk, tempt->tk);
+	if (current != NULL && current->next == NULL) {
+		a = strcmp(current->tk, tempt->tk);
 		if (a > 0) {
 			//insert before
-			tempt->next = previous;
-			indx->Array[indxPos] = tempt;
+			tempt->next = current;
+			indx->Array[indxPos].next = tempt;
 			return 1;
 		}
 		if (a < 0) {
 			//insert after
-			previous->next = tempt;
+			current->next = tempt;
 			tempt->next = NULL;
 			return 1;
 		}
 		if (a == 0) {
 			//fileNode* prevFile = previous->fileNodePTR;
 			if (prevFile != NULL && prevFile->next == NULL) {
-				int b = strcmp(prevFile->fileName, tempf->fileName);
-				if (b > 0) {
+				a = strcmp(prevFile->fileName, tempf->fileName);
+				if (a > 0) {
 					//insert before
 					tempf->next = prevFile;
-					previous->fileNodePTR = tempf;
+					current->fileNodePTR = tempf;
 					return 1;
 				}
-				if (b < 0) {
+				if (a < 0) {
 					//insert after
 					prevFile->next = tempf;
 					tempf->next = NULL;
 					return 1;
 				}
-				if (b == 0) {
+				if (a == 0) {
 					//increment token count in file
 					prevFile->Count++;
 					return 1;
@@ -89,14 +90,14 @@ int IndexInsert(Index *indx, char* tk, char* fileName) {
 			}
 			fileNode *currFile = prevFile->next;
 			while (currFile != NULL) {
-				int b = strcmp(currFile->fileName, tempf->fileName);
-				if (b > 0) {
+				a = strcmp(currFile->fileName, tempf->fileName);
+				if (a > 0) {
 					//insert before
 					tempf->next = currFile;
 					prevFile->next = tempf;
 					return 1;
 				}
-				if (b == 0) {
+				if (a == 0) {
 					//increment Count because same filename
 					currFile->Count++;
 					return 1;
@@ -113,81 +114,81 @@ int IndexInsert(Index *indx, char* tk, char* fileName) {
 		}
 		return 0;
 	}
-	fileNode *current = previous->next;
-	while (current != NULL) {
-		int a = strcmp(current->fileName, tempt->tk);
+	tkNode *next = current->next;
+	while (next != NULL) {
+		a = strcmp(current->tk, tempt->tk);
 		if (a > 0) {
 			//insert before because previous is larger than current
-			tempt->next = current;
-			previous->next = tempt;
+			tempt->next = next;
+			current->next = tempt;
 			return 1;
 		}
 		if (a == 0) {
-            int b = strcmp(prevFile, tempf);
+            		int b = strcmp(prevFile->fileName, tempf->fileName);
 			//same token, move through file list
-			fileNode *prevFile = current->fileName;
-			if (prevFile != NULL && prevFile->next == NULL) {
-				if (b > 0) {
-					//add before
-					tempf->next = prevFile;
-					previous->fileNodePTR = tempf;
-					return 1;
-				}
-				if (b < 0) {
-					//add after
-					prevFile->next = tempf;
-					tempf->next = NULL;
-					return 1;
-				}
-				if (b == 0) {
-					//increment token Count for file name
-					prevFile->Count++;
-					return 1;
-				}
-				return 0;
-			}
-			//make sure first file in list is smaller than given name
-			int c = strcmp(prevFile->fileName, tempf->fileName);
-			if (b > 0) {
-				//add before because first file is larger
-				tempf->next = prevFile;
-				current->fileName = tempf;
-				return 1;
-			}
-			fileNode *currFile = prevFile->next;
-			while (currFile != NULL) {
-				int b = strcmp(currFile->fileName, tempf->fileName);
+			fileNode *currFile = current->fileNodePTR;
+			if (currFile != NULL && currFile->next == NULL) {
 				if (b > 0) {
 					//add before
 					tempf->next = currFile;
-					prevFile->next = tempf;
-					return 1;
-
-				}
-				if (b == 0) {
-					//increment token Count
-					currFile->Count++;
+					current->fileNodePTR = tempf;
 					return 1;
 				}
-				if (currFile->next == NULL) {
+				if (b < 0) {
 					//add after
 					currFile->next = tempf;
 					tempf->next = NULL;
 					return 1;
 				}
-				prevFile = currFile;
-				currFile = currFile->next;
+				if (b == 0) {
+					//increment token Count for file name
+					currFile->Count++;
+					return 1;
+				}
+				return 0;
+			}
+			//make sure first file in list is smaller than given name
+			int c = strcmp(currFile->fileName, tempf->fileName);
+			if (c > 0) {
+				//add before because first file is larger
+				tempf->next = currFile;
+				currFile->next = tempf;
+				return 1;
+			}
+			fileNode *nextFile = currFile->next;
+			while (nextFile != NULL) {
+				int b = strcmp(nextFile->fileName, tempf->fileName);
+				if (b > 0) {
+					//add before
+					tempf->next = nextFile;
+					currFile->next = tempf;
+					return 1;
+
+				}
+				if (b == 0) {
+					//increment token Count
+					nextFile->Count++;
+					return 1;
+				}
+				if (currFile->next == NULL) {
+					//add after
+					nextFile->next = tempf;
+					tempf->next = NULL;
+					return 1;
+				}
+				currFile = nextFile;
+				nextFile = nextFile->next;
 			}
 		}
 		//if current->next is null add temp node to end
-		if (current->next = NULL) {
-			current->next = tempt;
+		if (next = NULL) {
+			next = tempt;
 			tempt->next = NULL;
 			return 1;
 		}
 		//if previous token is less than current token continue walk
-		previous = current;
-		current = current->next;
+		current = next;
+		next = next->next;
 	}
 }
 
@@ -269,7 +270,8 @@ int Hash(char c) {
 char* ReadFile(char* fileName) {
 	//if the file isn't there
 	if (fileName == NULL) {
-		return 1;
+		printf("file does not exist\n");
+		return NULL;
 	}
 	//make test variables for character and string, null out
 	char c;
@@ -293,14 +295,58 @@ char* ReadFile(char* fileName) {
 		return test;
 	}
 	if (a != 0) {
-		printf("cannot read from file");
+		printf("cannot read from file\n");
 		return NULL;
 	}
 }
-/*
-char* ReadDir(char* dirPath) {
 
-}*/
+int ReadDir(char* dirPath) {
+	char *dPath = dirPath;
+	char *fPath;
+	char* dName;
+	DIR *rDir;
+	struct dirent *dire;
+	char* output = (char*)calloc(1,(sizeof(char)));
+	//checks if directory path is a file
+	if((rDir = opendir(dPath)) == 0) {
+		//if so reads the file and returns the output
+		output = ReadFile(dirPath);
+		return 0;
+	}
+	//while directory entry is not null (visits each directory)
+	while((dire = readdir(rDir)) != 0) {
+		//if the type is a file
+		if (dire->dtype = DT_REG) {
+			//find the file path
+			fPath = dirPath;
+			fPath = Concat(dirPath, '/');
+			fPath = ConcatStr(fPath, dire->d_name);
+			//read and output the file
+			output = ReadFile(fPath);
+		}
+		//if the type is a directory
+		if (dire->d_type = DT_DIR) {
+			//check if directory is . or ..
+			dName = dire->d_name;
+			if (strcmp(dName, '.') == 0 || strcmp(dName, '.') == 0) {
+				//if so continue looping
+				continue;
+			} else {
+				//concat path and / for standard file path format
+				dPath = Concat(dPath, '/');
+				//concat path and directory name to keep format
+				dPath = ConcatStr(dPath, dire->d_name);
+				//recursively read from the new path
+				ReadDir(dPath);
+				//reset the path afterward
+				dPath = dirPath;
+			}
+		}
+	}
+	//close the directory
+	closedir(rDir);
+	return 0;
+}
 //done
 char* Concat(char* string, char c) {
 	int length = strlen(string);
@@ -315,7 +361,7 @@ char* ConcatStr(char* p1, char* p2) {
 	test = strcat(p1, p2);
 	return test;
 }
-
+//done no compile errors
 void IndexDestroy(Index *indx) {
 	//check if there is any data in hash table
 	if (indx == NULL) {
--- indexer.h
+++ indexer.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
 /*index hashtable*/
 struct Indexer {
